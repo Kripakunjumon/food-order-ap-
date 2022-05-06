0c4a861c8b7d7bcd2860f64bc2312546731714d3
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-05-06 17:50:30 +0500

    - improved tags ui

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
 import { Tag } from '../shared/models/tag';
 
@@ -9,9 +10,21 @@ import { Tag } from '../shared/models/tag';
 })
 export class TagsComponent implements OnInit {
   tags: Tag[] = [];
-  constructor(private foodService: FoodService) {}
+  searchItem: string = 'all';
+  constructor(
+    private foodService: FoodService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.tags = this.foodService.getAllTag();
+
+    this.route.params.subscribe((params) => {
+      if (params['tag']) {
+        this.searchItem = params['tag'];
+      } else {
+        this.searchItem = 'all';
+      }
+    });
   }
 }
