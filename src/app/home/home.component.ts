7aaefdf28e6185f653c3e4717f4ab1424b85819e
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-05-08 01:45:03 +0500

    - redeploy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
-
+ 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
